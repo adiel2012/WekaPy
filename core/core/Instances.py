aede16921a4208e2b437900afc7c7b3c925015a6
adiel castano <adiel2008@gmail.com>
2018-12-23 17:43:56 -0500

    adding load dataset and display

--- core/core/Instances.py
+++ core/core/Instances.py
@@ -1,15 +1,21 @@
 class Instances:
 
-    def __init__(self, values, classes, num_classes = 1):
+    def __init__(self, values, classes):
         self.values = values
         self.classes = classes
-        self.num_classes = num_classes
+        self.num_classes = len(classes[0])
 
     def getNumInstances(self):
         return len(self.values)
 
     def getInstanceValues(self, index):
         return self.values[index]
+
+    def getNumClasses(self):
+        return len(self.classes[0])
+
+    def getNumAttributes(self):
+        return len(self.values[0])
 
     def getInstanceClasses(self, index):
         return self.classes[index]
@@ -19,3 +25,22 @@ class Instances:
 
     def getClasses(self):
         return self.classes
+
+    def __repr__(self):
+        num_instances = self.getNumInstances()
+        num_attributes = self.getNumAttributes()
+        num_classes = self.getNumClasses()
+        res = "Number of Classes: " + str(num_classes) +'\n'
+        res += "Number of Attributes: " + str(num_attributes) +'\n'
+        res +=  "Number of Instances: " + str(num_instances) +'\n'
+
+        for i in range(num_instances):
+            for j in range(num_attributes):
+                print self.values[i][j], ',',
+            print ' --> ',
+            for j in range(num_classes):
+                print self.classes[i][j], ',',
+            print ''
+
+
+        return res
--- core/filters/others/LoadDataset.py
+++ core/filters/others/LoadDataset.py
@@ -1,4 +1,10 @@
 from core.core.interfaces  import IFilter
+from core.core  import Instances 
+import os.path
+import numpy as np
+
+def clean(str):
+    return str.strip().replace(" ", "")
 
 class LoadDataset(IFilter.IFilter):
     def __init__(self):
@@ -8,4 +14,33 @@ class LoadDataset(IFilter.IFilter):
         return "LoadDataset"    
     #return an array of instances or only one instance
     def execute(self, pipeddata, arrOptions):
-        return "JAJA " + arrOptions[0]
+        dataset_name = arrOptions[0]
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(my_path, "..","..", "..", "datasets", "classification", dataset_name)
+
+        f = open(path, "r")
+        # use readlines to read all lines in the file
+        # The variable "lines" is a list containing all lines in the file
+        lines = f.readlines()
+        # close the file after reading the lines.
+        f.close()
+        colums_description = clean(lines[0]).split(',')
+        num_classes = 0
+        num_attributes = 0
+        for c in colums_description:
+            if(c == '0'):
+                num_attributes += 1
+            else:
+                num_classes += 1
+
+        num_records = len(lines)-1
+        values = [[float(0)] * num_attributes for i in range(num_records)] 
+        classes = [[float(0)] * num_classes for i in range(num_records)]
+        for ri in range(1,num_records+1):
+            vals = clean(lines[ri]).split(',')
+            for ind in range(num_attributes):
+                values[ri-1][ind] = float(vals[ind])
+            for ind in range(num_classes):
+                classes[ri-1][ind] = float(vals[num_attributes+ind])
+
+        return Instances.Instances(values, classes)
